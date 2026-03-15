e8ff1f2a6d42ad537668f10c67e648919a656919
Houria <houriahasbell@gmail.com>
2026-03-15 15:48:45 +0100

    feat(backend): cors update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL || '', 'http://localhost:3000'],
+    origin: [
+        "https://gentle-field-0c500fe1e.4.azurestaticapps.net",
+        'http://localhost:3000',
+        'http://localhost:5173'
+    ],
     methods: ['GET', 'POST'], 
     allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
